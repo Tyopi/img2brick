20f2432b76c10e45542c6344f71dc15656907aeb
lariviere <matheo.lariviere2111@gmail.com>
2025-10-17 16:38:51 +0200

    connexion

--- show_img.php
+++ show_img.php
@@ -17,27 +17,33 @@ if (isset($_GET['img']) && file_exists($_GET['img'])) {
     <h1>Voici ton image :</h1>
     <img src="<?php echo htmlspecialchars($image); ?>"><br>    
 	<a href="get_img.php">changer l'image</a><br>
-	<a href="connecte.php">validez l'image</a><br>
+	<?php echo "<a href='connecte.php?img=" . urlencode($_GET['img']) . "'>validez l'image</a><br>"; ?>
     <form method="post" enctype="multipart/form-data">
         <input type="submit" name="lancer">
     </form>
+	
     <?php
+	$image_blob = addslashes(file_get_contents($image));
     if (isset($_POST['lancer'])) {
-    	$sql = "INSERT INTO matheo.test_connexion (test) VALUES (:id_uti)";
-	$stmt = $pdo->prepare($sql);
-	$stmt->execute([
- 	   ':id_uti' => 1
-	]);
-    	echo "Insertion reussie !";
-    	echo $pdo->query("SELECT current_database();")->fetchColumn();
+		$sql = "SELECT * FROM get2brick WHERE image = :image_blob";
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindParam(':image_blob', $image_blob, PDO::PARAM_LOB);
+		$stmt->execute();
 
-    	if ($stmt) {
-        	echo "oui";
-    	} else {
-        	echo "non";
-        	print_r($stmt->errorInfo());
-    	}
-    }
-?>
+		if ($stmt->num_rows > 0) {
+			echo "resultat trouver";
+		} else {
+			echo "aucun résultat";
+		}
+    	$sql = "INSERT INTO get2brick.img (image) VALUES (:id_uti)";
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute([
+		':id_uti' => $image_blob
+		]);	
+		$lastId = $pdo->lastInsertId();
+		echo "Image insérée avec id_img = " . $lastId . "<br>";
+    }	
+	?>
+
 </html>
 
